e008836f56eba149c75b8bbf07ff1d30aee97bf2
lw000 <373102227@qq.com>
2020-09-20 20:37:54 +0800

    zinx游戏服务端框架学习

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.15
 require (
 	github.com/aceld/zinx v0.0.0-20200918102026-219040a04c98
 	github.com/golang/protobuf v1.3.3
+	github.com/spf13/pflag v1.0.3
+	github.com/spf13/viper v1.7.1
 )
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"github.com/aceld/zinx/ziface"
 	"github.com/aceld/zinx/znet"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"log"
 )
 
@@ -19,7 +22,21 @@ func (p *PingRouter) Handle(request ziface.IRequest) {
 }
 
 func main() {
+	// using standard library "flag" package
+	flag.Int("flag_name", 1234, "help message for flag_name")
+
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	err := viper.BindPFlags(pflag.CommandLine)
+	if err != nil {
+		log.Println(err)
+	}
+
+	value := viper.GetInt("flag_name")
+	log.Println(value)
+
 	s := znet.NewServer()
-	s.AddRouter(0, &PingRouter{})
+	s.AddRouter(1, &PingRouter{})
 	s.Serve()
 }
